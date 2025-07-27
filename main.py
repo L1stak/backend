794dccff245377ecf963f438d5a101c5b86e3777
L1stak <113133819+L1stak@users.noreply.github.com>
2025-07-28 04:44:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-
+app.get("/")
+async def mainpage():
+    return {
+        helloworld
+    }
 app.get("/server-list")
 def serverlist():
     return {
