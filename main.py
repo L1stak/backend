2b029c5c3ade40d5be56d045542c44d8ce429052
L1stak <113133819+L1stak@users.noreply.github.com>
2025-07-28 04:32:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+app.get("/server-list")
+def serverlist():
+    return {
+        "code": 200,
+        "data": {
+            "official": [{"name": "test official server","online": 0}],
+            "unofficial": [{"name": "test unofficial server","online": 0}]
+
+        }
+        
+    }
